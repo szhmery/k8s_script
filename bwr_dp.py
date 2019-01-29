ddce89e2f87a2772ae062f43f852676380dd6c5d
Zhaohui Sun <zhaohsun@cisco.com>
2019-01-29 11:57:01 +0000

    add bwr_dp.py modify debug_usscheduler.sh and k8s

---
+++ bwr_dp.py
@@ -0,0 +1,68 @@
+import json
+import sys
+import os
+import time
+import subprocess
+import shlex
+
+import pexpect
+
+CISCOCM = '4458.2945.45c4'
+INTELCM = "0050.f112.decc"
+
+def main():
+    for i in range(1, len(sys.argv)):
+        print sys.argv[i]
+    global count
+    global interv
+    switch = sys.argv[1]
+    cm_type = sys.argv[2]
+    # print 'switch: ' + switch
+    # print 'cm_type: ' + cm_type
+    CM_MAC = ''
+    sfid = 0
+    sid = 0
+    if cm_type == 'cisco':
+        CM_MAC = CISCOCM
+    elif cm_type == 'intel':
+        CM_MAC = INTELCM
+    else:
+        print 'Input cable modem type is not right. It should be cisco or intel.'
+        exit(0)
+    curl = "curl -X GET http://127.0.0.1:32307/v1/sf/cm/"+CM_MAC
+    p = subprocess.Popen(curl, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
+
+    result_txt = p.stdout.read()
+    result_json = json.loads(result_txt)
+
+    for sf in result_json:
+        if sf['sfkeys']['sfdir'] == 'SF_DIR_US':
+            if sf['sfkeys']['sftype'] == 'SF_TYPE_PRIMARY':
+                sid = sf['sfkeys']['sid']
+                continue
+            if sf['sfkeys']['sftype'] == 'SF_TYPE_SECONDARY':
+                sfid = sf['sfkeys']['sfid']
+                continue
+    print "CM:"+CM_MAC+" Secondary SFID: "+str(sfid)+" Primary SID: "+str(sid)
+
+    print 'kubectl exec -it cmts-dp-macl3vpp-0 vppctl'
+    cmd = "kubectl exec -it cmts-dp-macl3vpp-0 vppctl"
+
+    child = pexpect.spawn(cmd)
+    # print child.before
+    child.expect('vpp-dp#')
+    debug_main = 'usmac log set enable main'
+    enable_cli = 'usmac test set bwr enable sfid ' + str(sfid) + ' sid ' + str(sid)
+    child.sendline(debug_main)
+    print child.before
+    # time.sleep(1)
+    child.expect('vpp-dp#')
+    child.sendline(enable_cli)
+    # time.sleep(1)
+    print child.before
+    child.expect('vpp-dp#')
+    print child.before
+
+
+if __name__ == "__main__":
+    main()
---
+++ debug_usscheduler.sh
@@ -0,0 +1,141 @@
+#!/bin/bash
+
+readonly PROGNAME=$(basename $0)
+readonly PROGDIR=$(readlink -m $(dirname $0))
+readonly ARGS="$@"
+
+cmdline() {
+    local arg=
+    for arg
+    do
+        local delim=""
+        case "$arg" in
+            --enable)               args="${args}-e ";;
+            --disable)              args="${args}-d ";;
+            *) [[ "${arg:0:1}" == "-" ]] || delim="\""
+                args="${args}${delim}${arg}${delim} ";;
+        esac
+    done
+ eval set -- $args
+
+ while getopts "hde" OPTION
+    do
+         case $OPTION in
+         h)
+             usage
+             exit 0
+             ;;
+         e)
+             readonly OPERATION="enable"
+             ;;
+
+         d)
+             readonly OPERATION="disable"
+             ;;
+         \?)
+             echo "Invalid option: $OPTARG"
+             usage
+             exit 0
+             ;;
+
+        esac
+    done
+    shift $((OPTIND -1))
+}
+
+usage() {
+    cat <<- EOF
+
+  usage: $PROGNAME [-h]
+  usage: $PROGNAME OPTIONS
+
+  This script will help to enable or disable cmts-rt-usscheduler debug log.
+
+  -h --help                        show this help
+
+  OPTIONS:
+     -e --enable                 enable debug
+     -d --disable                disable debug
+
+
+  Examples:
+     ./$PROGNAME -e
+     ./$PROGNAME -d
+
+EOF
+}
+
+main() {
+    local rc=0
+    cmdline ${ARGS}
+
+    echo "Start to get usscheduler IP!"
+    echo "kubectl get pods -o wide | grep ussche"
+    USSCHEDULER_IP=`kubectl get pods -o wide | grep ussche | awk '{print $6}'`
+    echo "usscheduler IP address is  $USSCHEDULER_IP"
+
+    # enable debug for bwr
+    if [ -n "$OPERATION" ]
+    then
+    if [ $OPERATION == "enable" ]
+        then
+
+        curl -X PUT -d 'ON'   --noproxy $USSCHEDULER_IP $USSCHEDULER_IP:8080/debug/bwr
+        echo "open debug bwr"
+        curl -X PUT -d 'ON'   --noproxy  $USSCHEDULER_IP $USSCHEDULER_IP:8080/debug/rtt
+        echo "open debug rtt"
+        curl -X PUT -d 'ON'   --noproxy  $USSCHEDULER_IP $USSCHEDULER_IP:8080/debug/grant
+        echo "open debug grant"
+        curl -X PUT -d 'ON'   --noproxy  $USSCHEDULER_IP $USSCHEDULER_IP:8080/debug/api
+        echo "open debug api"
+        curl -X PUT -d 'DEBUG' --noproxy $USSCHEDULER_IP $USSCHEDULER_IP:8080/debug/level
+        echo "debug level: DEBUG"
+        curl -X PUT -d 'ERROR' --noproxy $USSCHEDULER_IP $USSCHEDULER_IP:8080/debug/sublevel/TELEMETRY
+        echo "TELEMETRY debug level: ERROR"
+        curl -X PUT -d 'ERROR' --noproxy $USSCHEDULER_IP $USSCHEDULER_IP:8080/debug/sublevel/UMP
+        echo "UMP debug level: ERROR"
+        curl -X PUT -d 'ERROR' --noproxy $USSCHEDULER_IP $USSCHEDULER_IP:8080/debug/sublevel/MAPBD
+        echo "MAPBD debug level: ERROR"
+        curl -X PUT -d 'DEBUG' --noproxy $USSCHEDULER_IP $USSCHEDULER_IP:8080/debug/sublevel/KAFKA
+        echo "KAFKA debug level: DEBUG"
+
+        echo "Open usscheduler debug finished!"
+
+    fi
+
+
+    if [ $OPERATION == "disable" ]
+        then
+
+        curl -X PUT -d 'OFF'   --noproxy $USSCHEDULER_IP $USSCHEDULER_IP:8080/debug/bwr
+        echo "disable debug bwr"
+        curl -X PUT -d 'OFF'   --noproxy  $USSCHEDULER_IP $USSCHEDULER_IP:8080/debug/rtt
+        echo "disable debug rtt"
+        curl -X PUT -d 'OFF'   --noproxy  $USSCHEDULER_IP $USSCHEDULER_IP:8080/debug/grant
+        echo "disable debug grant"
+        curl -X PUT -d 'OFF'   --noproxy  $USSCHEDULER_IP $USSCHEDULER_IP:8080/debug/api
+        echo "disable debug api"
+        curl -X PUT -d 'ON'   --noproxy  $USSCHEDULER_IP $USSCHEDULER_IP:8080/debug/error
+        echo "enable debug error"
+        curl -X PUT -d 'ERROR' --noproxy $USSCHEDULER_IP $USSCHEDULER_IP:8080/debug/level
+        echo "debug level: ERROR"
+        curl -X PUT -d 'ERROR' --noproxy $USSCHEDULER_IP $USSCHEDULER_IP:8080/debug/sublevel/TELEMETRY
+        echo "TELEMETRY debug level: ERROR"
+        curl -X PUT -d 'ERROR' --noproxy $USSCHEDULER_IP $USSCHEDULER_IP:8080/debug/sublevel/UMP
+        echo "UMP debug level: ERROR"
+        curl -X PUT -d 'ERROR' --noproxy $USSCHEDULER_IP $USSCHEDULER_IP:8080/debug/sublevel/MAPBD
+        echo "MAPBD debug level: ERROR"
+        curl -X PUT -d 'ERROR' --noproxy $USSCHEDULER_IP $USSCHEDULER_IP:8080/debug/sublevel/KAFKA
+        echo "KAFKA debug level: ERROR"
+
+        echo "Disable usscheduler debug finished!"
+
+
+    fi
+  fi
+  rc=$(($rc+$?))
+  result=$?
+  return $result
+}
+
+main
